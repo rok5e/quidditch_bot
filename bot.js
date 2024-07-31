2a7943e0e068e39526a2234f28a8e0c55fcadb0d
adamovichg <88191236+adamovichg@users.noreply.github.com>
2024-07-31 17:42:39 +0200

    Added custom bot commands and a server with a port so Render will stop complaining

--- bot.js
+++ bot.js
@@ -1,4 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000; // Use the port provided by Render or default to 3000
 
 // Telegram bot token
 const token = process.env.TELEGRAM_BOT_TOKEN;
@@ -6,19 +10,37 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, { polling: true });
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // Send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
-
-// Listen for any kind of message. There are different kinds of messages.
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
+  const messageText = msg.text.toLowerCase();
+  
+  // Check if it's the user's first message
+  if (!userMessages.has(chatId)) {
+    bot.sendMessage(chatId, 'Crucio! Just kidding :)\nHow can I enchant you?');
+    userMessages.add(chatId);
+  }
+});
 
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
+// Handle /start command
+bot.onText(/\/start/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Crucio! Just kidding :)\nHow can I enchant you?');
+});
+
+// Handle /help command
+bot.onText(/\/help/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'These are the spells you can use:\n/start - Bring me to life\n/help - Retrieve your list of spells\n/avada kedavra - Try to kill me\n/lumos - light a candle\n/accio snitch - Call the snitch to play Quidditch');
+});
+
+// Handle /avada kedavra command
+bot.onText(/\/avada\skedavra/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Ha! That doesn\'t affect me puny human!');
+});
+
+// Handle /lumos command
+bot.onText(/\/lumos/, (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, ':candle:!');
 });
